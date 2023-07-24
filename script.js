daf98dce03ffa18143c0480579495545649b7ffa
Matheus <matheusteodorogarcia@gmail.com>
2023-07-24 15:24:33 -0300

    Accordion Menu em Faq

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu img')
-const tabDescricao = document.querySelectorAll('.js-tabdescricao div')
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu img')
+  const tabDescricao = document.querySelectorAll('.js-tabdescricao div')
 
-if (tabMenu.length && tabDescricao.length) {
-  function activeTab(index) {
-    tabDescricao.forEach((value) => {
-      value.classList.remove('active')
+  if (tabMenu && tabDescricao) {
+    function activeTab(index) {
+      tabDescricao.forEach((value) => {
+        value.classList.remove('active')
+      })
+      tabDescricao[index].classList.add('active')
+    }
+
+    tabMenu.forEach((value, index) => {
+      value.addEventListener('click', () => {
+        activeTab(index)
+      })
     })
-    tabDescricao[index].classList.add('active')
   }
-
-  tabMenu.forEach((value, index) => {
-    value.addEventListener('click', () => {
-      activeTab(index)
-    })
-  })
 }
+initTabNav()
+
+function initAccordion() {
+  const faqLista = document.querySelectorAll('.js-accordion dt')
+  if (faqLista) {
+    faqLista.forEach((value) => {
+      value.addEventListener('click', () => {
+        value.classList.toggle('active-accordion')
+      })
+    })
+  }
+}
+initAccordion()
